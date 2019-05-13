536757333d60fff653c035dc1dc1c349eeeb8368
Franco Reyes <freyes1988@gmail.com>
2019-05-13 16:31:45 -0500

    Add validations and test to model

--- app/models/average_bus_stop_boarding.rb
+++ app/models/average_bus_stop_boarding.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class AverageBusStopBoarding < ApplicationRecord
+  validates :on_street, presence: true
+  validates :cross_street, presence: true
+  validates :routes, presence: true, numericality: true
+  validates :boardings, presence: true, numericality: true
+  validates :alightings, presence: true, numericality: true
+  validates :month_beginning, presence: true
+  validates :daytype, presence: true
+  validates :longitude, presence: true, numericality: true
+  validates :latitude, presence: true, numericality: true
 end
--- spec/models/average_bus_stop_boarding_spec.rb
+++ spec/models/average_bus_stop_boarding_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AverageBusStopBoarding, type: :model do
+  it { is_expected.to validate_numericality_of :routes }
+  it { is_expected.to validate_numericality_of :boardings }
+  it { is_expected.to validate_numericality_of :alightings }
+  it { is_expected.to validate_numericality_of :longitude }
+  it { is_expected.to validate_numericality_of :latitude }
+
   it { is_expected.to validate_presence_of :on_street }
   it { is_expected.to validate_presence_of :cross_street }
   it { is_expected.to validate_presence_of :routes }
