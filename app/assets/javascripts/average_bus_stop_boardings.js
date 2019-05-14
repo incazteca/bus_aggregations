d84ca06eae7425c907b9970a21d36335fe02a781
Franco Reyes <freyes1988@gmail.com>
2019-05-13 23:18:20 -0500

    WIP of d3 use

--- app/assets/javascripts/average_bus_stop_boardings.js
+++ app/assets/javascripts/average_bus_stop_boardings.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+route_aggregation_chart = {
+  const svg = d3.select(DOM.svg(width, height));
+
+  const bar = svg.append("g")
+    .attr("fill","steelblue")
+
+}
+
--- app/controllers/average_bus_stop_boardings_controller.rb
+++ app/controllers/average_bus_stop_boardings_controller.rb
@@ -1,5 +1,16 @@
 class AverageBusStopBoardingsController < ApplicationController
   def index
+    @numeric_columns = AverageBusStopBoarding.columns.map(&:name)
     @bus_stops = AverageBusStopBoarding
+  end
+
+  def route_aggregation
+    AverageBusStopBoarding.route_aggregates
+  end
+
+  private
+
+  def route_aggregation_params
+    params.permit(:aggregate_function, :field)
   end
 end
