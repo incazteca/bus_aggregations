86e47e6ea90629c00b7a20072d541824f8d4b0b6
Franco Reyes <freyes1988@gmail.com>
2019-05-16 00:27:03 -0500

    Begin adding tests for controller

--- spec/controllers/average_bus_stop_boardings_controller_spec.rb
+++ spec/controllers/average_bus_stop_boardings_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AverageBusStopBoardingsController, type: :controller do
+  describe 'GET #index' do
+    let(:default_params) do
+      {
+        aggregate: {
+          field: 'boardings',
+          function: 'count',
+          group: 'routes',
+          limit: 20,
+          offset: 0
+        }
+      }
+    end
 
+    before { get(:index, params: default_params) }
+
+    it { is_expected.to render_template 'index' }
+  end
 end
