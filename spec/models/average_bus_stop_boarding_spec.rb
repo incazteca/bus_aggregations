5b9a05e618c5a68ab629223f184eaaa64d59bd2e
Franco Reyes <freyes1988@gmail.com>
2019-05-15 20:22:04 -0500

    Start adding tests

--- spec/models/average_bus_stop_boarding_spec.rb
+++ spec/models/average_bus_stop_boarding_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe AverageBusStopBoarding, type: :model do
   it { is_expected.to validate_presence_of :daytype }
   it { is_expected.to validate_presence_of :longitude }
   it { is_expected.to validate_presence_of :latitude }
+
+  describe '.route_aggregates' do
+    before do
+      create(:addison_route, cross_street: 'HARLEM', boardings: 40, alightings: 5)
+      create(:addison_route, cross_street: 'OAK PARK', boardings: 30, alightings: 10)
+      create(:addison_route, cross_street: 'NORMANDY', boardings: 20, alightings: 15)
+      create(:addison_route, cross_street: 'NEENAH', boardings: 10, alightings: 20)
+
+      create(:harlem_route, cross_street: 'ADDISON', boardings: 75, alightings: 20)
+      create(:harlem_route, cross_street: 'IRVING PARK', boardings: 25, alightings: 30)
+    end
+  end
 end
